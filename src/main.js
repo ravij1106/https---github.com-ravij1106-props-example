ebb2b0d4b9aeb354b00a59c4b7c3a7e1a03951e2
ravijoshi1106 <ravi.bh.java@gmail.com>
2021-01-12 12:10:08 +0530

    changed main js file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
-
 import App from './App.vue';
-//import FriendContact from './components/FriendContact.vue';
 
 Vue.config.productionTip = false
 
  new Vue({
-   comments : {
-     //FriendContact
-   },
    render: h => h(App),
  }).$mount('#app')
